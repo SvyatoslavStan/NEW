49d8b9b011208ae33468145747bb24188195df44
StanislavKhilyuk <geffie@yandex.ru>
2023-06-12 15:47:21 +0300

    added task01 task02 task03 task05 and gitignore

---
+++ task01/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string username = Console.ReadLine();
+        Console.WriteLine("Привет, ");
+        Console.WriteLine(username);
+    }
+}
